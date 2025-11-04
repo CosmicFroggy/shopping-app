422b86bf7bb69436681ee7a3e43ab4c49d753e6e
CosmicFroggy <mark.southern98@gmail.com>
2025-11-04 14:15:09 +0000

    fix error with endpoints

--- backend/src/main/java/com/shopping/backend/controller/ListingController.java
+++ backend/src/main/java/com/shopping/backend/controller/ListingController.java
@@ -6,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopping.backend.Entity.Listing;
 import com.shopping.backend.service.ListingService;
 
 
-@RestController("/listing")
+@RestController
+@RequestMapping("/listing")
 public class ListingController {
     
     @Autowired

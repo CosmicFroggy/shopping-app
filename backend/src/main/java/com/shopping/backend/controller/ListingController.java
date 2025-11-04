fc19402a36fb63ee027e7f3d2f8c467e1ba490ca
CosmicFroggy <mark.southern98@gmail.com>
2025-11-04 14:57:59 +0000

    change entity folder name

--- backend/src/main/java/com/shopping/backend/controller/ListingController.java
+++ backend/src/main/java/com/shopping/backend/controller/ListingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shopping.backend.Entity.Listing;
+import com.shopping.backend.entity.Listing;
 import com.shopping.backend.service.ListingService;
 
 
---
+++ backend/src/main/java/com/shopping/backend/entity/Listing.java
@@ -0,0 +1,25 @@
+package com.shopping.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Listing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+    
+}

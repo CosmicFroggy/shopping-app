4feca60b54da10597a90b76eb6004435504d2224
CosmicFroggy <mark.southern98@gmail.com>
2025-11-05 11:01:51 +0000

    add cors for frontend

--- backend/src/main/java/com/shopping/backend/controller/ListingController.java
+++ backend/src/main/java/com/shopping/backend/controller/ListingController.java
@@ -1,9 +1,10 @@
-package com.shopping.backend.controller;
 
+package com.shopping.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shopping.backend.entity.Listing;
 import com.shopping.backend.service.ListingService;
 
-
 @RestController
 @RequestMapping("/listing")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ListingController {
     
     @Autowired

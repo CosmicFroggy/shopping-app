b53846f849385a597e92f0c70d21241378a12070
CosmicFroggy <mark.southern98@gmail.com>
2025-11-10 17:26:26 +0000

    add repo annotation

--- backend/src/main/java/com/shopping/backend/repository/ListingRepository.java
+++ backend/src/main/java/com/shopping/backend/repository/ListingRepository.java
@@ -1,8 +1,10 @@
 package com.shopping.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.shopping.backend.entity.Listing;
 
+@Repository
 public interface ListingRepository extends JpaRepository<Listing, Long> {
 }
--- backend/src/main/java/com/shopping/backend/repository/UserRepository.java
+++ backend/src/main/java/com/shopping/backend/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.shopping.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.shopping.backend.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 }

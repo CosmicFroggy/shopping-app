99f071f034bff23c33d8a590c7f05b8cf99f24e4
CosmicFroggy <mark.southern98@gmail.com>
2025-11-11 10:37:14 +0000

    allow listing requests and disable csrf

--- backend/src/main/java/com/shopping/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/shopping/backend/config/SecurityConfig.java
@@ -41,7 +41,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .csrf(csrf -> csrf.disable()) // not needed in rest api
+            .authorizeHttpRequests(auth -> 
+                auth
+                    .requestMatchers("/listing/**").permitAll()
+                    .anyRequest().authenticated())
             .httpBasic(Customizer.withDefaults());
         return http.build();
     }  

d253634379c95a70da6d100593297f65fbcc9442
CosmicFroggy <mark.southern98@gmail.com>
2025-11-11 15:00:49 +0000

    create separate AuthRequestDTO

--- backend/src/main/java/com/shopping/backend/controller/AuthController.java
+++ backend/src/main/java/com/shopping/backend/controller/AuthController.java
@@ -4,17 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shopping.backend.dto.AuthRequestDTO;
 import com.shopping.backend.service.JWTService;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -28,16 +24,8 @@ public class AuthController {
     @Autowired
     private JWTService jwtService;
     
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    private class AuthRequest {
-        private String username;
-        private String password;
-    }
-    
     @PostMapping()
-    public String authenticate(@RequestBody AuthRequest authRequest) {
+    public String authenticate(@RequestBody AuthRequestDTO authRequest) {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
             return jwtService.generateToken(authRequest.getUsername());
---
+++ backend/src/main/java/com/shopping/backend/dto/AuthRequestDTO.java
@@ -0,0 +1,14 @@
+package com.shopping.backend.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequestDTO {
+    
+    private String username;
+    private String password;
+}

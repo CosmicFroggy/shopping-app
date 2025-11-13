15d1daffe8607c5dc5b3532c0e76ca85b4a10bbe
CosmicFroggy <mark.southern98@gmail.com>
2025-11-13 15:42:27 +0000

    fix ts for ListingForm component

--- frontend/src/components/ListingForm.tsx
+++ frontend/src/components/ListingForm.tsx
@@ -1,48 +1,62 @@
 import { useState } from "react";
 
-const ListingForm = ({ createListing }) => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
+type props = {
+    createListing: (listing: {
+        name: string;
+        description: string;
+        price: number;
+    }) => void;
+};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    createListing({
-      name: name,
-      description: description,
-      price: parseFloat(price),
-    });
-    setName("");
-    setDescription("");
-    setPrice("");
-  };
+const ListingForm = ({ createListing }: props) => {
+    const [name, setName] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [price, setPrice] = useState<string>("");
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="text"
-        name="description"
-        placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <input
-        type="number"
-        name="price"
-        placeholder="Price"
-        value={price}
-        onChange={(e) => setPrice(e.target.value)}
-      />
-      <button type="submit">Create Listing</button>
-    </form>
-  );
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        createListing({
+            name: name,
+            description: description,
+            price: parseFloat(price),
+        });
+        setName("");
+        setDescription("");
+        setPrice("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={name}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                    setName(e.target.value)
+                }
+            />
+            <input
+                type="text"
+                name="description"
+                placeholder="Description"
+                value={description}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                    setDescription(e.target.value)
+                }
+            />
+            <input
+                type="number"
+                name="price"
+                placeholder="Price"
+                value={price}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                    setPrice(e.target.value)
+                }
+            />
+            <button type="submit">Create Listing</button>
+        </form>
+    );
 };
 
 export default ListingForm;

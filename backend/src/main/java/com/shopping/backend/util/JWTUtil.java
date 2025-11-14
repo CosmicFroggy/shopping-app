f03fb67de6159ad201f868822dd82eaf6fbb2605
CosmicFroggy <mark.southern98@gmail.com>
2025-11-14 14:24:05 +0000

    remove unused import

--- backend/src/main/java/com/shopping/backend/util/JWTUtil.java
+++ backend/src/main/java/com/shopping/backend/util/JWTUtil.java
@@ -1,14 +1,7 @@
 package com.shopping.backend.util;
-
 import java.util.Date;
-
 import javax.crypto.SecretKey;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import com.shopping.backend.service.UserService;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;

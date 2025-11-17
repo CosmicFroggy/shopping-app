ac5293cab526233dbff80c4dca21226fb0a91923
CosmicFroggy <mark.southern98@gmail.com>
2025-11-17 15:33:18 +0000

    add jwt retrieval to frontend

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,12 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "../auth/useAuth";
+import { useNavigate } from "react-router-dom";
+
+type UserInfo = {
+    username: string;
+    password: string;
+};
 
 const LoginPage = () => {
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const { token, setToken } = useAuth();
+    const navigate = useNavigate();
+
+    // if token isn't null then return to home, they're already logged in
+    useEffect(() => {
+        if (token) {
+            navigate("/");
+        }
+    }, [token, navigate]);
+
+    const login = async (userInfo: UserInfo): Promise<void> => {
+        try {
+            const res: Response = await fetch("http://localhost:8080/auth", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userInfo),
+            });
+
+            if (!res.ok) {
+                throw new Error(
+                    `Could not create user? Request response status: ${res.status}`,
+                );
+            }
+
+            setToken(await res.text());
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error(error.message);
+            }
+        }
+    };
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
-        console.log("login attempt!");
+        const userInfo: UserInfo = { username, password };
+        login(userInfo);
         setUsername("");
         setPassword("");
     };

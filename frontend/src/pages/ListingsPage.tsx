8b02d2add36a03a6e0408b70b509331a680a61ba
CosmicFroggy <mark.southern98@gmail.com>
2025-11-17 13:33:41 +0000

    add token to requests on frontend

--- frontend/src/pages/ListingsPage.tsx
+++ frontend/src/pages/ListingsPage.tsx
@@ -11,14 +11,18 @@ const ListingsPage = () => {
     useEffect(() => {
         const getListings = async (): Promise<void> => {
             try {
+                // set authorisation header if the token exists
+                const headers: Record<string, string> = {};
+                if (token) {
+                    headers.Authorization = `Bearer ${token}`;
+                }
+
                 // TODO: learn axios
                 const res: Response = await fetch(
                     "http://localhost:8080/listing",
                     {
                         method: "GET",
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        },
+                        headers,
                     },
                 );
 
@@ -42,12 +46,16 @@ const ListingsPage = () => {
 
     const createListing = async (listingInfo: ListingInfo): Promise<void> => {
         try {
+            // set authorisation header if the token exists
+            const headers: Record<string, string> = {};
+            if (token) {
+                headers.Authorization = `Bearer ${token}`;
+            }
+            headers["Content-Type"] = "application/json";
+
             const res: Response = await fetch("http://localhost:8080/listing", {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
+                headers,
                 body: JSON.stringify(listingInfo),
             });
 
@@ -68,13 +76,17 @@ const ListingsPage = () => {
 
     const deleteListingById = async (id: number): Promise<void> => {
         try {
+            // set authorisation header if the token exists
+            const headers: Record<string, string> = {};
+            if (token) {
+                headers.Authorization = `Bearer ${token}`;
+            }
+
             const res: Response = await fetch(
                 `http://localhost:8080/listing/${id}`,
                 {
                     method: "DELETE",
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
+                    headers,
                 },
             );
 

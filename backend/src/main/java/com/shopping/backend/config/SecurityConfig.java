925be574e5007c0985245860d1ba982b027b8456
CosmicFroggy <mark.southern98@gmail.com>
2025-11-18 16:51:02 +0000

    allow requests to registration

--- backend/src/main/java/com/shopping/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/shopping/backend/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable()) // not needed in rest api
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**", "/h2/**" ).permitAll()
+                .requestMatchers("/auth/**", "/h2/**, /user/register/**" ).permitAll()
                 .anyRequest().authenticated())
             .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // h2 console breaks because spring security blocks iframes
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);  // add our custom jwt filter that will authenticate requests with a jwt token

ec2064fce4960be71eef745a8c514b26bd2c12be
CosmicFroggy <mark.southern98@gmail.com>
2025-11-19 12:45:04 +0000

    add login error handling to backend

--- backend/src/main/java/com/shopping/backend/controller/AuthController.java
+++ backend/src/main/java/com/shopping/backend/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.shopping.backend.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.shopping.backend.dto.AuthRequestDto;
 import com.shopping.backend.dto.AuthResponseDto;
+import com.shopping.backend.dto.ErrorResponseDto;
 import com.shopping.backend.service.AuthService;
 
 @RestController
@@ -20,10 +25,26 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<AuthResponseDto> authenticate(@RequestBody AuthRequestDto authRequest) {
         String token = authService.authenticate(authRequest);
         AuthResponseDto response = new AuthResponseDto(token);
         return ResponseEntity.ok(response);
+    }
+
+    // TODO: create global exception handler?
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponseDto> handleBadCredentials(BadCredentialsException error) {
+        return ResponseEntity
+            .status(HttpStatus.UNAUTHORIZED)
+            .body(new ErrorResponseDto("Incorrect password."));
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleUsernameNotFound(UsernameNotFoundException error) {
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(new ErrorResponseDto("User does not exist."));
     }
 }

debe0edeac2646012d3ab886b88ad0b966e04529
CosmicFroggy <mark.southern98@gmail.com>
2025-11-25 16:07:38 +0000

    create sidebar component

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,11 @@
 import logo from "../assets/logo.png";
 import account from "../assets/account.png";
 import { useState } from "react";
-import { useAuth } from "../auth/useAuth";
 import { Link } from "react-router-dom";
+import SideBar from "./SideBar";
 
 const NavBar = () => {
     const [open, setOpen] = useState<boolean>(false);
-    const { token, setToken } = useAuth();
 
     return (
         <div className="relative bg-amber-200 p-2.5 flex items-center justify-between">
@@ -24,23 +23,8 @@ const NavBar = () => {
                 onClick={() => setOpen(!open)}
             />
             {open && (
-                <div className="absolute top-full right-0 h-60 w-60 bg-blue-400 border-2 border-amber-200">
-                    <ol>
-                        <li hidden={!token}>
-                            <button onClick={() => setToken(null)}>
-                                Log out
-                            </button>
-                        </li>
-                        <li hidden={!token}>
-                            <Link to="/create">Create listing</Link>
-                        </li>
-                        <li hidden={!!token}>
-                            <Link to="/login">Log in</Link>
-                        </li>
-                        <li hidden={!!token}>
-                            <Link to="/signup">Sign up</Link>
-                        </li>
-                    </ol>
+                <div className="absolute top-full right-0">
+                    <SideBar />
                 </div>
             )}
         </div>

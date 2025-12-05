0afb298fd95cd271e0dcc8a5ed9fadd6187fd027
CosmicFroggy <mark.southern98@gmail.com>
2025-12-05 15:59:24 +0000

    add sorting to frontend

--- frontend/src/pages/ListingsPage.tsx
+++ frontend/src/pages/ListingsPage.tsx
@@ -8,6 +8,8 @@ const ListingsPage = () => {
     const [listings, setListings] = useState<Listing[]>([]);
     const { token } = useAuth();
 
+    const [sorting, setSorting] = useState<string>("");
+
     useEffect(() => {
         const getListings = async (): Promise<void> => {
             try {
@@ -21,7 +23,7 @@ const ListingsPage = () => {
                 const backendPort: number =
                     parseInt(import.meta.env.VITE_BACKEND_PORT) || 8080;
                 const res: Response = await fetch(
-                    `http://localhost:${backendPort}/listing`,
+                    `http://localhost:${backendPort}/listing?${sorting}`,
                     {
                         method: "GET",
                         headers,
@@ -45,7 +47,7 @@ const ListingsPage = () => {
         };
 
         getListings();
-    }, []);
+    }, [sorting]);
 
     const deleteListingById = async (id: number): Promise<void> => {
         try {
@@ -85,6 +87,23 @@ const ListingsPage = () => {
                 listings={listings}
                 deleteListingByID={deleteListingById}
             />
+            <form>
+                <label htmlFor="sorting">Sort by:</label>
+                <select
+                    value={sorting}
+                    onChange={(
+                        event: React.ChangeEvent<HTMLSelectElement>,
+                    ): void => setSorting(event.target.value)}
+                    name="sorting"
+                    id="sorting"
+                >
+                    <option value="">None</option>
+                    <option value="sort=price,desc">Price: high to low</option>
+                    <option value="sort=price,asc">Price: low to high</option>
+                    <option value="sort=name,asc">Name: A to Z</option>
+                    <option value="sort=name,desc">Name: Z to A</option>
+                </select>
+            </form>
         </div>
     );
 };

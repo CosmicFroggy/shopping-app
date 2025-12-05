253dcd888f0363e0bebb67b8add17eb483fa1082
CosmicFroggy <mark.southern98@gmail.com>
2025-12-05 12:16:28 +0000

    remove pid text file generation

--- backend/src/main/java/com/shopping/backend/BackendApplication.java
+++ backend/src/main/java/com/shopping/backend/BackendApplication.java
@@ -2,14 +2,12 @@ package com.shopping.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(BackendApplication.class);
-		springApplication.addListeners(new ApplicationPidFileWriter());
 		springApplication.run(args);
 	}
 

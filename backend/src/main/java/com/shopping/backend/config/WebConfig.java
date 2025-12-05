903b8ffb73c4a939d44c626c549d7f7119d3be57
CosmicFroggy <mark.southern98@gmail.com>
2025-12-05 12:52:54 +0000

    add HATEOAS to get pagination links

--- backend/src/main/java/com/shopping/backend/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.shopping.backend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-@Configuration
-@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO) // ensures consistency of pagineated json response between versions
-public class WebConfig {
-}
--- backend/src/main/java/com/shopping/backend/controller/ListingController.java
+++ backend/src/main/java/com/shopping/backend/controller/ListingController.java
@@ -1,8 +1,10 @@
 
 package com.shopping.backend.controller;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopping.backend.entity.Listing;
@@ -27,13 +28,11 @@ public class ListingController {
         this.listingService = listingService;
     }
 
-    @GetMapping
-    public Page<Listing> getAll(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size) {
-    
-        Pageable pageable = PageRequest.of(page, size);
-        return listingService.getAll(pageable);
+    // use HATEOAS PagedModel to return next, previous, etc. links
+    @GetMapping                                                                       // assembler is method injected here
+    public ResponseEntity<PagedModel<EntityModel<Listing>>> getAll(Pageable pageable, PagedResourcesAssembler<Listing> assembler) {
+        Page<Listing> listings = listingService.getAll(pageable);
+        return ResponseEntity.ok(assembler.toModel(listings));
     }
 
     @GetMapping("/{id}")

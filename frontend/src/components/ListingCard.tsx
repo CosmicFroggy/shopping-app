73a390d28b3050aa10156eff78264f334a07a574
CosmicFroggy <mark.southern98@gmail.com>
2025-12-10 11:25:19 +0000

    restrict some frontend features to admin

--- frontend/src/components/ListingCard.tsx
+++ frontend/src/components/ListingCard.tsx
@@ -1,4 +1,5 @@
 import type { Listing } from "../types/Listing";
+import { useAuth } from "../auth/useAuth";
 
 interface Props {
     listing: Listing;
@@ -6,12 +7,15 @@ interface Props {
 }
 
 const ListingCard = ({ listing, onDelete }: Props) => {
+    const { authInfo } = useAuth();
+
     return (
         <li className="listing-card relative">
             <h3 className="listing-card-title">{listing.name}</h3>
             <p>{listing.description}</p>
             <p className="italic">£{listing.price}</p>
             <button
+                hidden={authInfo?.role !== "ADMIN"}
                 className="shadow-md rounded-md bg-white border border-red-500 p-0.5"
                 onClick={() => onDelete()}
             >
--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -5,12 +5,20 @@ const SideBar = () => {
     const { authInfo, setAuthInfo } = useAuth();
 
     return (
-        <div className="h-60 w-60 bg-blue-400 border-2 border-amber-200">
+        <div className="p-3 h-60 w-60 bg-blue-400 border-2 border-amber-200">
+            <div className="mb-2">
+                <h1 className="font-semibold text-lg" hidden={!authInfo}>
+                    {authInfo?.username}
+                </h1>
+                <h2 className="italic text-sm" hidden={!authInfo}>
+                    {authInfo?.role}
+                </h2>
+            </div>
             <ol>
                 <li hidden={!authInfo}>
                     <button onClick={() => setAuthInfo(null)}>Log out</button>
                 </li>
-                <li hidden={!authInfo}>
+                <li hidden={authInfo?.role !== "ADMIN"}>
                     <Link to="/create">Create listing</Link>
                 </li>
                 <li hidden={!!authInfo}>

e01e51caefa60bbadc04b05a2d9b5f049e8b5368
CosmicFroggy <mark.southern98@gmail.com>
2025-12-11 09:58:36 +0000

    add login as basic user test

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -28,4 +28,28 @@ test.describe("login tests", () => {
 
         //TODO: also check that admin permissions is correct, i.e that create and delete listings buttons are there, or is this a separate test?
     });
+
+    test("login as basic user", async ({ page }) => {
+        const loginPage: LoginPage = new LoginPage(page);
+        const listingsPage: ListingsPage = new ListingsPage(page);
+
+        await loginPage.goto();
+        await loginPage.login("user", "user1234");
+
+        // check navigation to home
+        await expect(page).toHaveURL(baseURL + "/");
+
+        // check user logged in
+        await expect(listingsPage.navbar.accountButton).toBeVisible();
+        await listingsPage.navbar.openAccountMenu();
+        await expect(listingsPage.navbar.accountMenu.root).toBeVisible();
+        await expect(listingsPage.navbar.accountMenu.usernameTag).toHaveText(
+            "user",
+        );
+        await expect(listingsPage.navbar.accountMenu.roleTag).toHaveText(
+            "USER",
+        );
+
+        //TODO: also check that basic user permissions are correct, i.e that create and delete listings buttons are not there, or is this a separate test?
+    });
 });
